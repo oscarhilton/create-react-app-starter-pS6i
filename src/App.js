d366a7832bfdfe17ecca86cea727cc7f99984b3e
Replit user <>
2023-11-20 16:39:38 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+const ThreeHourTimer = () => {
+  const [startTime, setStartTime] = useState(Date.now());
+  const [endTime, setEndTime] = useState(Date.now() + 3 * 60 * 60 * 1000); // 3 hours in milliseconds
+  const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = Date.now();
+      if (now >= endTime) {
+        clearInterval(interval);
+        setPercent(100);
+      } else {
+        const newPercent = ((now - startTime) / (endTime - startTime)) * 100;
+        setPercent(newPercent);
+      }
+    }, 1000); // update every second
+
+    return () => clearInterval(interval);
+  }, [startTime, endTime]);
+
+  return (
+    <div>
+      <div>Time Remaining: {((endTime - Date.now()) / 1000 / 60).toFixed(2)} minutes</div>
+      <div>Progress: {percent.toFixed(2)}%</div>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
+      <ThreeHourTimer />
     </div>
   );
 }
